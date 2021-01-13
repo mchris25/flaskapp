f91862e0f8a964c252f7027d7afcd48fd8ddf793
Max Christy <47064300+mchris25@users.noreply.github.com>
2021-01-13 13:13:14 -0800

    First commit of my flask app

--- sample_backend.py
+++ sample_backend.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import request
 app = Flask(__name__)
 
 @app.route('/')
@@ -6,6 +7,21 @@ def hello_world():
     return 'Hello, World!'
 @app.route('/users')
 def get_users():
+   search_username = request.args.get('name') #accessing the value of parameter 'name'
+   if search_username :
+      subdict = {'users_list' : []}
+      for user in users['users_list']:
+         if user['name'] == search_username:
+            subdict['users_list'].append(user)
+      return subdict
+   return users
+@app.route('/users/<id>')
+def get_user(id):
+   if id :
+      for user in users['users_list']:
+        if user['id'] == id:
+           return user
+      return ({})
    return users
 
 users = { 
